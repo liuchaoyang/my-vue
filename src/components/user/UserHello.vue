37f52d6bd912564bdd189eafe9f13f36861fe114
liuchaoyang <875818589@qq.com>
2018-12-24 13:51:39 +0800

    use axios

--- src/components/user/UserHello.vue
+++ src/components/user/UserHello.vue
@@ -1,10 +1,53 @@
 <template>
-    <div>user hello</div>
+    <div>user hello
+        <div v-if="loading">Loading...</div>
+
+        <div
+                v-else
+                v-for="currency in info"
+                class="currency"
+        >
+            {{ currency.description }}:
+            <span class="lighten">
+        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+      </span>
+        </div>
+
+    </div>
 </template>
 
 <script>
+    import Vue from 'vue'
+    import axios from 'axios'
+
+    Vue.use(axios)
+
     export default {
-        name: "user-list"
+        name: "user-hello",
+        data () {
+            return {
+                info: null,
+                loading: true,
+                errored: false
+            }
+        },
+        filters: {
+            currencydecimal (value) {
+                return value.toFixed(2)
+            }
+        },
+        mounted () {
+            axios
+                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                .then(response => {
+                    this.info = response.data.bpi
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.errored = true
+                })
+                .finally(() => this.loading = false)
+        }
     }
 </script>
 
