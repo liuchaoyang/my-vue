65a8ab2f8453807a01b5999cff0a1aa96ea7f7b6
liuchaoyang <chaoyang.liu@innotree.cn>
2019-02-25 02:24:36 +0800

    详情10张图片

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -15,7 +15,7 @@
                            :auto-upload="false"
                            :before-upload="beforeUpload">
                     <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                    <div slot="tip" class="el-upload__tip">高450，宽750，只能上传jpg/png文件，且不超过500kb</div>
                 </el-upload>
 
             </el-form-item>
@@ -32,7 +32,7 @@
                            :auto-upload="false"
                            :before-upload="beforeUpload">
                     <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                    <div slot="tip" class="el-upload__tip">高450，宽750，只能上传jpg/png文件，且不超过500kb</div>
                 </el-upload>
 
             </el-form-item>
@@ -49,7 +49,7 @@
                            :auto-upload="false"
                            :before-upload="beforeUpload">
                     <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                    <div slot="tip" class="el-upload__tip">高450，宽750，只能上传jpg/png文件，且不超过500kb</div>
                 </el-upload>
 
             </el-form-item>
--- src/components/user/UserList.vue
+++ src/components/user/UserList.vue
@@ -8,7 +8,7 @@
         <el-table :data="tableData" border >
             <el-table-column prop="id" label="商品编号"></el-table-column>
             <el-table-column prop="name" label="商品名称" ></el-table-column>
-            <el-table-column prop="summary" label="简介" ></el-table-column>
+            <!--<el-table-column prop="summary" label="简介" ></el-table-column>-->
             <el-table-column prop="yprice" label="原价" ></el-table-column>
             <el-table-column prop="price" label="现格" ></el-table-column>
             <el-table-column prop="createTime" label="创建时间" width="100"></el-table-column>
@@ -17,6 +17,7 @@
             <el-table-column fixed="right" label="操作" >
                 <template slot-scope="scope">
                     <el-button @click="handleClick(scope.row)" type="text" size="small">编辑</el-button>
+                    <el-button @click="handleDetailClick(scope.row)" type="text" size="small">详情上传</el-button>
                 </template>
             </el-table-column>
         </el-table>
@@ -49,7 +50,7 @@
                                :auto-upload="false"
                                :before-upload="beforeUpload">
                         <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-                        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                        <div slot="tip" class="el-upload__tip">高450，宽750，只能上传jpg/png文件，且不超过500kb</div>
                     </el-upload>
 
                 </el-form-item>
@@ -66,7 +67,7 @@
                                :auto-upload="false"
                                :before-upload="beforeUpload">
                         <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-                        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                        <div slot="tip" class="el-upload__tip">高450，宽750，只能上传jpg/png文件，且不超过500kb</div>
                     </el-upload>
 
                 </el-form-item>
@@ -83,7 +84,7 @@
                                :auto-upload="false"
                                :before-upload="beforeUpload">
                         <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-                        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                        <div slot="tip" class="el-upload__tip">高450，宽750，只能上传jpg/png文件，且不超过500kb</div>
                     </el-upload>
 
                 </el-form-item>
@@ -92,6 +93,160 @@
                 <el-button @click="addFormVisible = false">取 消</el-button>
                 <el-button type="primary" @click="save">确 定</el-button>
             </div>
+        </el-dialog>
+
+
+
+
+
+        <!--详情 dialog-->
+        <el-dialog title="详情图片上传" :visible.sync="detailFormVisible">
+            <el-form>
+                <el-tag type="danger">图片要求：尺寸:高450，宽75, 大小不超过500kb</el-tag>
+                <el-form-item label="图片1" :label-width="formLabelWidth">
+                    <el-upload class="avatar-uploader"
+                               action=""
+                               :on-change="detailChangeEvt0"
+                               :show-file-list="true"
+                               :multiple="false"
+                               :limit="1"
+                               :file-list="detailPath"
+                               :auto-upload="false"
+                               :before-upload="beforeUpload">
+                        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                    </el-upload>
+
+                </el-form-item>
+                <el-form-item label="图片2" :label-width="formLabelWidth">
+                    <el-upload class="avatar-uploader"
+                               action=""
+                               :on-change="detailChangeEvt1"
+                               :show-file-list="true"
+                               :multiple="false"
+                               :limit="1"
+                               :file-list="detailPath"
+                               :auto-upload="false"
+                               :before-upload="beforeUpload">
+                        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                    </el-upload>
+
+                </el-form-item>
+
+                <el-form-item label="图片3" :label-width="formLabelWidth">
+                    <el-upload class="avatar-uploader"
+                               action=""
+                               :on-change="detailChangeEvt2"
+                               :show-file-list="true"
+                               :multiple="false"
+                               :limit="1"
+                               :file-list="detailPath"
+                               :auto-upload="false"
+                               :before-upload="beforeUpload">
+                        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                    </el-upload>
+
+                </el-form-item>
+
+                <el-form-item label="图片4" :label-width="formLabelWidth">
+                    <el-upload class="avatar-uploader"
+                               action=""
+                               :on-change="detailChangeEvt3"
+                               :show-file-list="true"
+                               :multiple="false"
+                               :limit="1"
+                               :file-list="detailPath"
+                               :auto-upload="false"
+                               :before-upload="beforeUpload">
+                        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                    </el-upload>
+
+                </el-form-item>
+
+                <el-form-item label="图片5" :label-width="formLabelWidth">
+                    <el-upload class="avatar-uploader"
+                               action=""
+                               :on-change="detailChangeEvt4"
+                               :show-file-list="true"
+                               :multiple="false"
+                               :limit="1"
+                               :file-list="detailPath"
+                               :auto-upload="false"
+                               :before-upload="beforeUpload">
+                        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                    </el-upload>
+
+                </el-form-item>
+                <el-form-item label="图片6" :label-width="formLabelWidth">
+                    <el-upload class="avatar-uploader"
+                               action=""
+                               :on-change="detailChangeEvt5"
+                               :show-file-list="true"
+                               :multiple="false"
+                               :limit="1"
+                               :file-list="detailPath"
+                               :auto-upload="false"
+                               :before-upload="beforeUpload">
+                        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                    </el-upload>
+
+                </el-form-item>
+                <el-form-item label="图片7" :label-width="formLabelWidth">
+                    <el-upload class="avatar-uploader"
+                               action=""
+                               :on-change="detailChangeEvt6"
+                               :show-file-list="true"
+                               :multiple="false"
+                               :limit="1"
+                               :file-list="detailPath"
+                               :auto-upload="false"
+                               :before-upload="beforeUpload">
+                        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                    </el-upload>
+
+                </el-form-item>
+                <el-form-item label="图片8" :label-width="formLabelWidth">
+                    <el-upload class="avatar-uploader"
+                               action=""
+                               :on-change="detailChangeEvt7"
+                               :show-file-list="true"
+                               :multiple="false"
+                               :limit="1"
+                               :file-list="detailPath"
+                               :auto-upload="false"
+                               :before-upload="beforeUpload">
+                        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                    </el-upload>
+
+                </el-form-item>
+                <el-form-item label="图片9" :label-width="formLabelWidth">
+                    <el-upload class="avatar-uploader"
+                               action=""
+                               :on-change="detailChangeEvt8"
+                               :show-file-list="true"
+                               :multiple="false"
+                               :limit="1"
+                               :file-list="detailPath"
+                               :auto-upload="false"
+                               :before-upload="beforeUpload">
+                        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                    </el-upload>
+
+                </el-form-item>
+                <el-form-item label="图片10" :label-width="formLabelWidth">
+                    <el-upload class="avatar-uploader"
+                               action=""
+                               :on-change="detailChangeEvt9"
+                               :show-file-list="true"
+                               :multiple="false"
+                               :limit="1"
+                               :file-list="detailPath"
+                               :auto-upload="false"
+                               :before-upload="beforeUpload">
+                        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                    </el-upload>
+
+                </el-form-item>
+            </el-form>
         </el-dialog>
     </div>
 </template>
@@ -116,6 +271,7 @@
                     logo: ''
                 },
                 addFormVisible: false,
+                detailFormVisible: false,
                 formLabelWidth: '120px'
             }
         },
@@ -192,6 +348,70 @@
                 this.bannerPath = [];
                 this.detailPath = [];
                 this.addFormVisible = true;
+            },
+            handleDetailClick: function (row) {
+                this.form.id = row.id;
+                this.form.name = row.name;
+                this.form.summary = row.summary;
+                this.form.yprice = row.yprice;
+                this.form.price = row.price;
+
+                this.logoPath = [];
+                this.bannerPath = [];
+                this.detailPath = [];
+                this.detailFormVisible = true;
+            },
+
+
+            //详情图片上传
+            detailChangeEvt0 (file, fileList) {
+                this.uploadDetailFile(0, fileList);
+            },
+            detailChangeEvt1 (file, fileList) {
+                this.uploadDetailFile(1, fileList);
+            },
+            detailChangeEvt2 (file, fileList) {
+                this.uploadDetailFile(2, fileList);
+            },
+            detailChangeEvt3 (file, fileList) {
+                this.uploadDetailFile(3, fileList);
+            },
+            detailChangeEvt4 (file, fileList) {
+                this.uploadDetailFile(4, fileList);
+            },
+            detailChangeEvt5 (file, fileList) {
+                this.uploadDetailFile(5, fileList);
+            },
+            detailChangeEvt6 (file, fileList) {
+                this.uploadDetailFile(6, fileList);
+            },
+            detailChangeEvt7 (file, fileList) {
+                this.uploadDetailFile(7, fileList);
+            },
+            detailChangeEvt8 (file, fileList) {
+                this.uploadDetailFile(8, fileList);
+            },
+            detailChangeEvt9 (file, fileList) {
+                this.uploadDetailFile(9, fileList);
+            },
+            uploadDetailFile(index, fileList) {
+                let formData = new FormData();
+                formData.append('file', fileList[0] ? fileList[0].raw : '');
+                formData.append('productId', this.form.id);
+                const instance = axios.create({
+                    // baseURL: 'https://www.yiyadr.com/my-doctor/',
+                    baseURL: 'http://localhost:8086/',
+                    headers: {
+                        "Content-Type": "multipart/form-data"
+                    }
+                });
+                instance.post('/product/detail/upload/'+index, formData)
+                    .then(response => {
+                        console.log(response.data);
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
             },
             refreshTable: function () {
                 axios
